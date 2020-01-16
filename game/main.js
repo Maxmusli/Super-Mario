71369521d4ee8d2a022c6fa1cdda947b60f25467
max <maxmus2223@gmail.com>
2020-01-15 21:28:01 -0800

    add start game overlay

--- game/main.js
+++ game/main.js
@@ -7,49 +7,55 @@ import { loadEntities } from './entities.js';
 import Entity from './entity.js';
 import PlayerController from './traits/player_controller.js';
 
-function createPlayerEnv(playerEntity) {
-  const playerEnv = new Entity();
-  const playerControl = new PlayerController();
-  playerControl.checkpoint.set(64, 64);
-  playerControl.setPlayer(playerEntity);
-  playerEnv.addTrait(playerControl);
-  return playerEnv;
+export default class GameMain {
+  start() {
+    function createPlayerEnv(playerEntity) {
+      const playerEnv = new Entity();
+      const playerControl = new PlayerController();
+      playerControl.checkpoint.set(64, 64);
+      playerControl.setPlayer(playerEntity);
+      playerEnv.addTrait(playerControl);
+      return playerEnv;
+    }
+    
+    const canvas = document.getElementById('screen');
+    const context = canvas.getContext('2d');
+    
+    Promise.all([
+      loadEntities(),
+      loadLevel('1-1'),
+    ])
+      .then(([[createMario, createGoomba, createKoopa], level]) => {
+        const camera = new Camera();
+        window.camera = camera;
+    
+        const mario = createMario();
+    
+        const goomba = createGoomba();
+        goomba.pos.x = 220;
+    
+        const koopa = createKoopa();
+        koopa.pos.x = 250;
+    
+        level.entities.add(goomba);
+        level.entities.add(koopa);
+    
+        const playerEnv = createPlayerEnv(mario);
+        level.entities.add(playerEnv);
+    
+        const input = setupKeyboard(mario);
+        input.listenTo(window)
+        
+        const timer = new Timer(1/60)
+        timer.update = function update(deltaTime) {
+          level.update(deltaTime)
+          level.comp.draw(context, camera)
+          camera.pos.x = Math.max(0, mario.pos.x - 100)
+        }
+    
+        timer.start()    
+      })
+  }
+  
 }
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
-
-Promise.all([
-  loadEntities(),
-  loadLevel('1-1'),
-])
-  .then(([[createMario, createGoomba, createKoopa], level]) => {
-    const camera = new Camera();
-    window.camera = camera;
-
-    const mario = createMario();
-
-    const goomba = createGoomba();
-    goomba.pos.x = 220;
-
-    const koopa = createKoopa();
-    koopa.pos.x = 250;
-
-    level.entities.add(goomba);
-    level.entities.add(koopa);
-
-    const playerEnv = createPlayerEnv(mario);
-    level.entities.add(playerEnv);
-
-    const input = setupKeyboard(mario);
-    input.listenTo(window)
-    
-    const timer = new Timer(1/60)
-    timer.update = function update(deltaTime) {
-      level.update(deltaTime)
-      level.comp.draw(context, camera)
-      camera.pos.x = Math.max(0, mario.pos.x - 100)
-    }
-
-    timer.start()    
-  })
